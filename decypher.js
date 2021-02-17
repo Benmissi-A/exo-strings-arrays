557a91dcc6c3acdfe0440633593e78d07a9a1730
Benmissi-A <aminebenmissi@hotmail.fr>
2021-02-17 18:04:51 +0100

    decypher dont work

---
+++ decypher.js
@@ -0,0 +1,26 @@
+let crypted = 'YRMV PFL VMVI YRU R UIVRD EVF KYRK PFL NVIV JF JLIV NRJ IVRC NYRK ZW PFL NVIV LERSCV KF NRBV WIFD KYRK UIVRD YFN NFLCU PFL BEFN KYV UZWWVIVETV SVKNVVE KYV UIVRD NFICU REU KYV IVRC NFICU'
+
+let splitCrypted = crypted.split(' ')
+
+let uncrypted = ''
+
+//console.log(splitCrypted)
+
+
+let word = ''
+
+for(let i =  -24; i < 1 ; ++i){
+  let nb=i
+  for(let index = 0 ; index < splitCrypted.length ; ++index){
+    uncrypted = ''
+    for(let char = 0 ; char < splitCrypted[index].length ; ++char ){
+    uncrypted += String.fromCharCode(splitCrypted[index].charCodeAt(char)+nb)
+  }
+  word += uncrypted+' '
+}
+console.log(word)
+word=''
+console.log('')
+console.log('')
+console.log('')
+}
