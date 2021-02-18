5831fad6a552fddab4aef9ead678985de14a0e74
Benmissi-A <aminebenmissi@hotmail.fr>
2021-02-18 01:48:22 +0100

    decypher is ok

--- decypher.js
+++ decypher.js
@@ -1,26 +1,21 @@
 let crypted = 'YRMV PFL VMVI YRU R UIVRD EVF KYRK PFL NVIV JF JLIV NRJ IVRC NYRK ZW PFL NVIV LERSCV KF NRBV WIFD KYRK UIVRD YFN NFLCU PFL BEFN KYV UZWWVIVETV SVKNVVE KYV UIVRD NFICU REU KYV IVRC NFICU'
 
 let splitCrypted = crypted.split(' ')
+let uncryptedChar = ''
+let uncryptedMsg= ''
 
-let uncrypted = ''
-
-//console.log(splitCrypted)
-
-
-let word = ''
-
-for(let i =  0; i < 1 ; ++i){
-  let nb=i
+//for(let i =  0; i < 26 ; ++i){                   // boucle initiale qui teste toutes les lettres
+  let nb=9                                        // la valeur est i , ja'mis 9 car c'est le bon decalage
   for(let index = 0 ; index < splitCrypted.length ; ++index){
-    uncrypted = ''
+    uncryptedChar = ''
     for(let char = 0 ; char < splitCrypted[index].length ; ++char ){
-    uncrypted += String.fromCharCode(splitCrypted[index].charCodeAt(char)-nb)
+      let x = splitCrypted[index].charCodeAt(char)
+      x = ((x-65+nb)%26) // formule magique pour le decalage un modulo du nombre de lettres permet de revenir au debut de l'alphabet
+      uncryptedChar += String.fromCharCode(x+65)
   }
-  word += uncrypted+' '
+  uncryptedMsg += uncryptedChar+' '
 }
-console.log(word)
+console.log(uncryptedMsg)
 word=''
-console.log('')
-console.log('')
-console.log('')
-}
+//}
+
